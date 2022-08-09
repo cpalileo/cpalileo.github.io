96309793784ce075c933aef2307464af93eba3c5
Christopher Palileo <cpalileo@gmail.com>
2022-08-09 10:32:22 -0700

    updated About page with name and portrait

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import coverImage from "../../assets/cover/design.jpg";
+import coverImage from "../../assets/img-about/portrait.jpg";
 
 // The About() below is the meat of the component that returns the JSX code which will be used for rendering in the app.js file
 function About() {
   return (
     <section className="my-5">
-      <h1 id="about">About</h1>
+      <h1 id="about">Christopher Palileo</h1>
       <img
         src={coverImage}
         className="my-2"
