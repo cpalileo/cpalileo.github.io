721fa46498f41fbea8d54178d7c590cadfd803b1
Christopher Palileo <cpalileo@gmail.com>
2022-08-12 23:03:07 -0700

    hardcoded current projects with test images

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+import photo from "../../assets/small/commercial/0.jpg";
 
-function Projects() {
+function Projects(props) {
+  const currentCategory = {
+    name: "current projects",
+    description: "Projects I'm currently working on.",
+  };
+
   return (
     <section>
-      <h1>PROJECT NAME</h1>
-      <p>PROJECT DESCRIPTION</p>
-      <p>PROJECT SCREENSHOT</p>
-      <p>SPELLED OUT LINK TO LIVE</p>
-      <p>SPELLED OUT LINK TO REPO</p>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.name}</p>
+      <div className="flex-row">
+        <img
+          src={photo}
+          alt="Commercial Example"
+          className="img-thumbnail mx-1"
+        />
+      </div>
     </section>
   );
 }
