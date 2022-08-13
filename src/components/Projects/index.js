c561a52c9c90ca55027cc099dfed71cb841a7759
Christopher Palileo <cpalileo@gmail.com>
2022-08-12 22:40:43 -0700

    initial setup of Projects component

---
+++ src/components/Projects/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <section>
+      <h1>PROJECT NAME</h1>
+      <p>PROJECT DESCRIPTION</p>
+      <p>PROJECT SCREENSHOT</p>
+      <p>SPELLED OUT LINK TO LIVE</p>
+      <p>SPELLED OUT LINK TO REPO</p>
+    </section>
+  );
+}
+
+export default Projects;
