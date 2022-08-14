360d2610f53a0515dafa0f0a825dcc3f983612de
Christopher Palileo <cpalileo@gmail.com>
2022-08-13 23:31:13 -0700

    source control

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,9 +5,9 @@ function About() {
   return (
     <section className="my-5">
       <div className="my-2">
-        <div className="profile-img my-5">
+        {/* <div className="profile-img my-5">
           <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
-        </div>
+        </div> */}
         <img
           src={selfPortrait}
           style={{ width: "25%" }}
