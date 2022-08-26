9a4fb345ad3e659a0b965d9a2cbe01bd3a417518
Christopher Palileo <cpalileo@gmail.com>
2022-08-25 17:36:01 -0700

    fixed footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-
-  // Replace links with social media profiles
-  const icons = [
-    {
-      name: "fab fa-github",
-      link: "https://github.com/"
-    },
-    {
-      name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
-    },
-  ]
-
   return (
     <footer className="flex-row px-1">
-      {icons.map(icon =>
-      (
-        <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"><i className={icon.name}></i></a>
-      )
-        )}
+      <div class="parent">
+        <div>
+          <a href="https://www.github.com/">
+            <img
+              class="icon"
+              src={require(`../../assets/cover/github.png`)}
+              alt="GitHub Logo"
+            />
+          </a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/">
+            <img
+              class="icon"
+              src={require(`../../assets/cover/linkedin.png`)}
+              alt="LinkedIn Logo"
+            />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
