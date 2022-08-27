cce19acbf454d5d28acf167e9014731e8dccefa3
Christopher Palileo <cpalileo@gmail.com>
2022-08-27 08:43:33 -0700

    minor cleanup of header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,8 @@ import React from "react";
 function Header(props) {
   return (
     <header className="flex-row space-between px-1">
-      <h1>PostIt</h1>
-      {/* <img src={coverImage} alt="wooden background"></img> */}
+      {/* <h1>PostIt</h1>
+      <img src={coverImage} alt="wooden background"></img> */}
       {props.children}
     </header>
   );
